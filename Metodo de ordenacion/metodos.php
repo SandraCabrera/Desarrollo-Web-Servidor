5861c5eb24b6c0b9ee285014549b7045c6106f5b
SandraCabrera <sandracabreragarcia26@gmail.com>
2019-10-28 18:55:08 +0100

    Creación del formulario

---
+++ Metodo de ordenacion/metodos.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<?php
+/**
+ * Practica php creación
+ * de métodos de ordenación.
+ * Dos tipos diferentes.
+ */
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Métodos de ordenación</title>
+</head>
+<body>
+  <h1>Métodos de ordenación</h1>
+
+  <form method="post">
+    Creación del Array de forma aleatoria: <input type="number" name="random" min="0" max="5000">
+    <br>
+    <br>
+    Introduce lo numeros que desea ordenar: <input type="text" name="por_teclado">
+    <br>(separado por comas)
+    <br>
+    <br>
+    <input type="submit" value="Ordenar">
+  </form>
+</body>
+</html>
