6de4128630d79a4380a8a8ad350afea6acba912a
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 16:46:31 +0100

    6.9 Send data to server

--- AjaxCrud/app/products/create-product.js
+++ AjaxCrud/app/products/create-product.js
@@ -74,5 +74,23 @@ $(document).ready(function(){
     $(document).on('submit', '#create-product-form', function(){
         // get form data
         var form_data=JSON.stringify($(this).serializeObject());
+
+        // submit form data to api
+        $.ajax({
+            url: "http://localhost/api/product/create.php",
+            type : "POST",
+            contentType : 'application/json',
+            data : form_data,
+            success : function(result) {
+                // product was created, go back to products list
+                showProducts();
+            },
+            error: function(xhr, resp, text) {
+                // show error to console
+                console.log(xhr, resp, text);
+            }
+        });
+        
+        return false;
     });
 });
