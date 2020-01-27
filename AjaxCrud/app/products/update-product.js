cb4ad6b0f5c2df9f021b50961b87dc9b67254558
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 18:06:23 +0100

    8.3 Read product information

--- AjaxCrud/app/products/update-product.js
+++ AjaxCrud/app/products/update-product.js
@@ -4,6 +4,19 @@ $(document).ready(function(){
     $(document).on('click', '.update-product-button', function(){
         // get product id
         var id = $(this).attr('data-id');
+
+        // read one record based on given product id
+        $.getJSON("http://localhost/api/product/read_one.php?id=" + id, function(data){
+        
+            // values will be used to fill out our form
+            var name = data.name;
+            var price = data.price;
+            var description = data.description;
+            var category_id = data.category_id;
+            var category_name = data.category_name;
+            
+            // load list of categories will be here
+        });
     });
      
     // 'update product form' submit handle will be here
