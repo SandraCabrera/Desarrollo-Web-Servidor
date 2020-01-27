637b3dfa2f3e20dcd1b052722638ba9850a6885a
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 17:45:53 +0100

    7.1 Handle "read one" button click

---
+++ AjaxCrud/app/products/read-one-product.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+ 
+    // handle 'read one' button click
+    $(document).on('click', '.read-one-product-button', function(){
+        // product ID will be here
+    });
+ 
+});
