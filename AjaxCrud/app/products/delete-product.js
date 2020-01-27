90dc93ab19abc8e79d9d85e8a79b411b675cc12d
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 18:14:06 +0100

    9.3 Show "delete confirmation" dialog box

--- AjaxCrud/app/products/delete-product.js
+++ AjaxCrud/app/products/delete-product.js
@@ -4,5 +4,24 @@ $(document).ready(function(){
     $(document).on('click', '.delete-product-button', function(){
         // get the product id
         var product_id = $(this).attr('data-id');
+
+        // bootbox for good looking 'confirm pop up'
+        bootbox.confirm({
+        
+            message: "<h4>Are you sure?</h4>",
+            buttons: {
+                confirm: {
+                    label: '<span class="glyphicon glyphicon-ok"></span> Yes',
+                    className: 'btn-danger'
+                },
+                cancel: {
+                    label: '<span class="glyphicon glyphicon-remove"></span> No',
+                    className: 'btn-primary'
+                }
+            },
+            callback: function (result) {
+                // delete request will be here
+            }
+        });
     });
 });
