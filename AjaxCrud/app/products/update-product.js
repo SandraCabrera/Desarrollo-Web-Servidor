159424807c4b2636649836c43793f01d82acfecb
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 18:05:27 +0100

    8.1 Handle "udpate product" button click

---
+++ AjaxCrud/app/products/update-product.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+ 
+    // show html form when 'update product' button was clicked
+    $(document).on('click', '.update-product-button', function(){
+        // product ID will be here
+    });
+     
+    // 'update product form' submit handle will be here
+});
