9d269b988e4e0684a972171bded5fe441ca73201
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 17:46:21 +0100

    7.2 Get product ID

--- AjaxCrud/app/products/read-one-product.js
+++ AjaxCrud/app/products/read-one-product.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
  
     // handle 'read one' button click
     $(document).on('click', '.read-one-product-button', function(){
-        // product ID will be here
+        // get product id
+        var id = $(this).attr('data-id');
     });
  
 });
