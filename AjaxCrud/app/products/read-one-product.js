32db9e8f1465f29e99907154bee73606be841b34
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 17:54:12 +0100

    7.5 Show record data in HTML table

--- AjaxCrud/app/products/read-one-product.js
+++ AjaxCrud/app/products/read-one-product.js
@@ -15,6 +15,35 @@ $(document).ready(function(){
         <div id='read-products' class='btn btn-primary pull-right m-b-15px read-products-button'>
             <span class='glyphicon glyphicon-list'></span> Read Products
         </div>
+
+        <!-- product data will be shown in this table -->
+        <table class='table table-bordered table-hover'>
+        
+            <!-- product name -->
+            <tr>
+                <td class='w-30-pct'>Name</td>
+                <td class='w-70-pct'>` + data.name + `</td>
+            </tr>
+        
+            <!-- product price -->
+            <tr>
+                <td>Price</td>
+                <td>` + data.price + `</td>
+            </tr>
+        
+            <!-- product description -->
+            <tr>
+                <td>Description</td>
+                <td>` + data.description + `</td>
+            </tr>
+        
+            <!-- product category name -->
+            <tr>
+                <td>Category</td>
+                <td>` + data.category_name + `</td>
+            </tr>
+        
+        </table>`;
     });
  
 });
