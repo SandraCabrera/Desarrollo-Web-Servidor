040dc0f523599b76dfa438896aa784097c7639e6
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 16:12:51 +0100

    5.2 Show products on click of a button

--- AjaxCrud/app/products/read-products.js
+++ AjaxCrud/app/products/read-products.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
  
     // show list of product on first load
     showProducts();
+
+    // when a 'read products' button was clicked
+    $(document).on('click', '.read-products-button', function(){
+        showProducts();
+    });
  
 });
  
