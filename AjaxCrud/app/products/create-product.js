94e1b6861fef484e1d917dd2abc8aa482327d814
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 16:43:30 +0100

    6.3 Build "categories option" select field

--- AjaxCrud/app/products/create-product.js
+++ AjaxCrud/app/products/create-product.js
@@ -4,7 +4,13 @@ $(document).ready(function(){
     $(document).on('click', '.create-product-button', function(){
         // load list of categories
         $.getJSON("http://localhost/Desarrollo-Web-Servidor/SimpleRestAPI/api/product/read.php", function(data){
-        
+            // build categories option html
+            // loop through returned list of data
+            var categories_options_html=`<select name='category_id' class='form-control'>`;
+            $.each(data.records, function(key, val){
+                categories_options_html+=`<option value='` + val.id + `'>` + val.name + `</option>`;
+            });
+            categories_options_html+=`</select>`;
         });
     });
  
