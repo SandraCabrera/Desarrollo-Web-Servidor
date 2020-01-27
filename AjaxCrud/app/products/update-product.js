58b662d6a991184a1101baff596c2e6a119a7e5c
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 18:09:41 +0100

    8.10 Send form data to server

--- AjaxCrud/app/products/update-product.js
+++ AjaxCrud/app/products/update-product.js
@@ -96,6 +96,22 @@ $(document).ready(function(){
         
         // get form data
         var form_data=JSON.stringify($(this).serializeObject()); 
+
+        // submit form data to api
+        $.ajax({
+            url: "http://localhost/api/product/update.php",
+            type : "POST",
+            contentType : 'application/json',
+            data : form_data,
+            success : function(result) {
+                // product was created, go back to products list
+                showProducts();
+            },
+            error: function(xhr, resp, text) {
+                // show error to console
+                console.log(xhr, resp, text);
+            }
+        });
         
         return false;
     });
