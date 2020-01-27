fd555c2f66a2219e3a60b8b8380273b9ea52a609
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 18:12:37 +0100

    9.1 Handle "Delete Product" button click

---
+++ AjaxCrud/app/products/delete-product.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+ 
+    // will run if the delete button was clicked
+    $(document).on('click', '.delete-product-button', function(){
+        // product id will be here
+    });
+});
