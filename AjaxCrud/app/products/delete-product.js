29451bd85bbe3e433e2796a7a20401c022089de0
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-27 18:13:08 +0100

    9.2 Get product ID

--- AjaxCrud/app/products/delete-product.js
+++ AjaxCrud/app/products/delete-product.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
  
     // will run if the delete button was clicked
     $(document).on('click', '.delete-product-button', function(){
-        // product id will be here
+        // get the product id
+        var product_id = $(this).attr('data-id');
     });
 });
