7ecbbb470a5f7282ff906a732b925b4ae5ef5768
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-28 16:23:07 +0100

    5.4 Use the create() method

--- rest-api-authentication-example/api/create_user.php
+++ rest-api-authentication-example/api/create_user.php
@@ -26,4 +26,28 @@ $user->lastname = $data->lastname;
 $user->email = $data->email;
 $user->password = $data->password;
  
-// use the create() method here
+// create the user
+if(
+    !empty($user->firstname) &&
+    !empty($user->email) &&
+    !empty($user->password) &&
+    $user->create()
+){
+ 
+    // set response code
+    http_response_code(200);
+ 
+    // display message: user was created
+    echo json_encode(array("message" => "User was created."));
+}
+ 
+// message if unable to create user
+else{
+ 
+    // set response code
+    http_response_code(400);
+ 
+    // display message: unable to create user
+    echo json_encode(array("message" => "Unable to create user."));
+}
+?>
