cd11a5f84debe44e1d1c3889ca0e314955111b04
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-28 16:24:50 +0100

    5.6 Add a create() method

--- rest-api-authentication-example/api/objects/user.php
+++ rest-api-authentication-example/api/objects/user.php
@@ -18,5 +18,42 @@ class User{
         $this->conn = $db;
     }
  
-// create() method will be here
+// create new user record
+function create(){
+ 
+    // insert query
+    $query = "INSERT INTO " . $this->table_name . "
+            SET
+                firstname = :firstname,
+                lastname = :lastname,
+                email = :email,
+                password = :password";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->firstname=htmlspecialchars(strip_tags($this->firstname));
+    $this->lastname=htmlspecialchars(strip_tags($this->lastname));
+    $this->email=htmlspecialchars(strip_tags($this->email));
+    $this->password=htmlspecialchars(strip_tags($this->password));
+ 
+    // bind the values
+    $stmt->bindParam(':firstname', $this->firstname);
+    $stmt->bindParam(':lastname', $this->lastname);
+    $stmt->bindParam(':email', $this->email);
+ 
+    // hash the password before saving to database
+    $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password_hash);
+ 
+    // execute the query, also check if query was successful
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+}
+ 
+// emailExists() method will be here
 }
