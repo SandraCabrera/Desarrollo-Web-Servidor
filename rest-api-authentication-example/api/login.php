d91a6a81cb4bc1cce2cacc1b90ab6d05f41f8455
SandraCabrera <sandracabreragarcia26@gmail.com>
2020-01-28 16:40:36 +0100

    6.5 Include files to encode JWT

--- rest-api-authentication-example/api/login.php
+++ rest-api-authentication-example/api/login.php
@@ -24,4 +24,12 @@ $data = json_decode(file_get_contents("php://input"));
 $user->email = $data->email;
 $email_exists = $user->emailExists();
  
-// files for jwt will be here
+// generate json web token
+include_once 'config/core.php';
+include_once 'libs/php-jwt-master/src/BeforeValidException.php';
+include_once 'libs/php-jwt-master/src/ExpiredException.php';
+include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once 'libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+ 
+// generate jwt will be here
